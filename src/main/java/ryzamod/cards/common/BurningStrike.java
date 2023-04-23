5a5085c750b4bca6d9ff1d849d2013a62b1d3da2
Rbcatz <rhib@tampabay.rr.com>
2023-04-23 04:50:55 -0400

    deleted common card comments

--- src/main/java/ryzamod/cards/common/BurningStrike.java
+++ src/main/java/ryzamod/cards/common/BurningStrike.java
@@ -15,9 +15,9 @@ import static ryzamod.RyzaMod.makeID;
 
 public class BurningStrike extends BaseCard {
     private final static CardInfo cardInfo = new CardInfo(
-            "BurningStrike", //Card ID. Will be prefixed with mod id, so the final ID will be "modID:MyCard" with whatever your mod's ID is.
-            1, //The card's base cost. -1 is X cost, -2 is no cost for unplayable cards like curses, or Reflex.
-            CardType.ATTACK, //The type. ATTACK/SKILL/POWER/CURSE/STATUS
+            "BurningStrike",
+            1,
+            CardType.ATTACK,
             CardTarget.ENEMY,
             CardRarity.COMMON,
             RyzaCharacter.Enums.CARD_COLOR
--- src/main/java/ryzamod/cards/common/Initiator.java
+++ src/main/java/ryzamod/cards/common/Initiator.java
@@ -19,24 +19,18 @@ import static ryzamod.RyzaMod.makeID;
 
 public class Initiator extends BaseCard {
     private final static CardInfo cardInfo = new CardInfo(
-            "Initiator", //Card ID. Will be prefixed with mod id, so the final ID will be "modID:MyCard" with whatever your mod's ID is.
-            0, //The card's base cost. -1 is X cost, -2 is no cost for unplayable cards like curses, or Reflex.
-            CardType.ATTACK, //The type. ATTACK/SKILL/POWER/CURSE/STATUS
-            CardTarget.ENEMY, //The target. Single target is ENEMY, all enemies is ALL_ENEMY. Look at cards similar to what you want to see what to use.
-            CardRarity.COMMON, //Rarity. BASIC is for starting cards, then there's COMMON/UNCOMMON/RARE, and then SPECIAL and CURSE. SPECIAL is for cards you only get from events. Curse is for curses, except for special curses like Curse of the Bell and Necronomicurse.
-            RyzaCharacter.Enums.CARD_COLOR //The card color. If you're making your own character, it'll look something like this. Otherwise, it'll be CardColor.RED or something similar for a basegame character color.
+            "Initiator",
+            0,
+            CardType.ATTACK,
+            CardTarget.ENEMY,
+            CardRarity.COMMON,
+            RyzaCharacter.Enums.CARD_COLOR
     );
 
 
-    //This is theoretically optional, but you'll want it. The ID is how you refer to the card.
-    //For example, to add a card to the starting deck, you need to use its ID.
-    //With this, you can just use 'MyCard.ID'. Without it, you'd have to type out
-    //'yourModID:MyCard' and make sure you don't make any mistakes, and you'd also have to update it
-    //if you decided to change the card's ID.
     public static final String ID = makeID(cardInfo.baseId);
 
-    //These will be used in the constructor. Technically you can just use the values directly,
-    //but constants at the top of the file are easy to adjust.
+
     private static final int DAMAGE = 3;
     private static final int UPG_DAMAGE = 2;
 
@@ -44,11 +38,11 @@ public class Initiator extends BaseCard {
     private static final int UPG_MAGIC = 1;
 
     public Initiator() {
-        super(cardInfo); //Pass the cardInfo to the BaseCard constructor.
+        super(cardInfo);
 
         this.isInnate = true;
         this.exhaust = true;
-        setDamage(DAMAGE, UPG_DAMAGE); //Sets the card's damage and how much it increases when upgraded.
+        setDamage(DAMAGE, UPG_DAMAGE);
         setMagic(MAGIC, UPG_MAGIC);
     }
 
