0e88570d23436ed846477dbb7fd3bff41b116975
Rbcatz <rhib@tampabay.rr.com>
2023-04-23 05:11:34 -0400

    deleted crafts card comments

--- src/main/java/ryzamod/cards/crafts/FortressArmor.java
+++ src/main/java/ryzamod/cards/crafts/FortressArmor.java
@@ -15,29 +15,22 @@ import static ryzamod.character.RyzaCharacter.Enums.CARD_COLOR;
 
 public class FortressArmor extends CraftCard {
     private final static CardInfo cardInfo = new CardInfo(
-            "FortressArmor", //Card ID. Will be prefixed with mod id, so the final ID will be "modID:MyCard" with whatever your mod's ID is.
-            0, //The card's base cost. -1 is X cost, -2 is no cost for unplayable cards like curses, or Reflex.
-            CardType.SKILL, //The type. ATTACK/SKILL/POWER/CURSE/STATUS
-            CardTarget.SELF, //The target. Single target is ENEMY, all enemies is ALL_ENEMY. Look at cards similar to what you want to see what to use.
-            CardRarity.UNCOMMON, //Rarity. BASIC is for starting cards, then there's COMMON/UNCOMMON/RARE, and then SPECIAL and CURSE. SPECIAL is for cards you only get from events. Curse is for curses, except for special curses like Curse of the Bell and Necronomicurse.
-            CARD_COLOR.COLORLESS //The card color. If you're making your own character, it'll look something like this. Otherwise, it'll be CardColor.RED or something similar for a basegame character color.
+            "FortressArmor",
+            0,
+            CardType.SKILL,
+            CardTarget.SELF,
+            CardRarity.UNCOMMON,
+            CARD_COLOR.COLORLESS
     );
 
 
-    //This is theoretically optional, but you'll want it. The ID is how you refer to the card.
-    //For example, to add a card to the starting deck, you need to use its ID.
-    //With this, you can just use 'MyCard.ID'. Without it, you'd have to type out
-    //'yourModID:MyCard' and make sure you don't make any mistakes, and you'd also have to update it
-    //if you decided to change the card's ID.
     public static final String ID = makeID(cardInfo.baseId);
 
-    //These will be used in the constructor. Technically you can just use the values directly,
-    //but constants at the top of the file are easy to adjust.
 
     private static final int BLOCK = 10;
 
     public FortressArmor() {
-        super(cardInfo); //Pass the cardInfo to the BaseCard constructor.
+        super(cardInfo);
 
         setBlock(BLOCK);
     }
