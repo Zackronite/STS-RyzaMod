ccb55a68273d5d89b6951a9fdeb011412dc001b2
Rbcatz <rhib@tampabay.rr.com>
2023-04-23 05:26:59 -0400

    deleted basic card comments

--- src/main/java/ryzamod/cards/Gather.java
+++ src/main/java/ryzamod/cards/Gather.java
@@ -25,24 +25,17 @@ import static ryzamod.RyzaMod.makeID;
 
 public class Gather extends BaseCard{
     private final static CardInfo cardInfo = new CardInfo(
-            "Gather", //Card ID. Will be prefixed with mod id, so the final ID will be "modID:MyCard" with whatever your mod's ID is.
-            0, //The card's base cost. -1 is X cost, -2 is no cost for unplayable cards like curses, or Reflex.
-            CardType.ATTACK, //The type. ATTACK/SKILL/POWER/CURSE/STATUS
-            CardTarget.SELF, //The target. Single target is ENEMY, all enemies is ALL_ENEMY. Look at cards similar to what you want to see what to use.
-            CardRarity.BASIC, //Rarity. BASIC is for starting cards, then there's COMMON/UNCOMMON/RARE, and then SPECIAL and CURSE. SPECIAL is for cards you only get from events. Curse is for curses, except for special curses like Curse of the Bell and Necronomicurse.
-            RyzaCharacter.Enums.CARD_COLOR //The card color. If you're making your own character, it'll look something like this. Otherwise, it'll be CardColor.RED or something similar for a basegame character color.
+            "Gather",
+            0,
+            CardType.ATTACK,
+            CardTarget.SELF,
+            CardRarity.BASIC,
+            RyzaCharacter.Enums.CARD_COLOR
     );
 
 
-    //This is theoretically optional, but you'll want it. The ID is how you refer to the card.
-    //For example, to add a card to the starting deck, you need to use its ID.
-    //With this, you can just use 'MyCard.ID'. Without it, you'd have to type out
-    //'yourModID:MyCard' and make sure you don't make any mistakes, and you'd also have to update it
-    //if you decided to change the card's ID.
     public static final String ID = makeID(cardInfo.baseId);
 
-    //These will be used in the constructor. Technically you can just use the values directly,
-    //but constants at the top of the file are easy to adjust.
 
     private static final int COST = 1;
 
@@ -50,7 +43,7 @@ public class Gather extends BaseCard{
     private static final int UPG_MAGIC = 2;
 
     public Gather() {
-        super(cardInfo); //Pass the cardInfo to the BaseCard constructor.
+        super(cardInfo);
 
         setMagic(MAGIC, UPG_MAGIC);
     }
@@ -61,15 +54,8 @@ public class Gather extends BaseCard{
 
     @Override
     public void use(AbstractPlayer p, AbstractMonster m) {
-        /*addToBot(new GatherMaterialAction(3, false, new ArrayList<MaterialCategory>(Arrays.asList(
-                MaterialCategory.LUMBER,
-                MaterialCategory.THREAD,
-                MaterialCategory.GUNPOWDER
-        ))));*/
         addToBot(new GatherMaterialAction(3, false));
-        // addToBot(new ApplyPowerAction(p, p, new MindfulnessPower(p, 1)));
     }
-
 
     @Override
     public AbstractCard makeCopy() { //Optional
