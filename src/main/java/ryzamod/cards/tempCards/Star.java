6ce0f688294e5f5be3e261c5ba4f1e1b1664b44d
Rbcatz <rhib@tampabay.rr.com>
2023-04-23 05:22:14 -0400

    deleted tempcard and uncommon comments

--- src/main/java/ryzamod/cards/tempCards/Star.java
+++ src/main/java/ryzamod/cards/tempCards/Star.java
@@ -18,31 +18,24 @@ import static ryzamod.character.RyzaCharacter.Enums.CARD_COLOR;
 
 public class Star extends BaseCard {
     private final static CardInfo cardInfo = new CardInfo(
-            "Star", //Card ID. Will be prefixed with mod id, so the final ID will be "modID:MyCard" with whatever your mod's ID is.
-            0, //The card's base cost. -1 is X cost, -2 is no cost for unplayable cards like curses, or Reflex.
-            CardType.ATTACK, //The type. ATTACK/SKILL/POWER/CURSE/STATUS
-            CardTarget.ENEMY, //The target. Single target is ENEMY, all enemies is ALL_ENEMY. Look at cards similar to what you want to see what to use.
-            CardRarity.SPECIAL, //Rarity. BASIC is for starting cards, then there's COMMON/UNCOMMON/RARE, and then SPECIAL and CURSE. SPECIAL is for cards you only get from events. Curse is for curses, except for special curses like Curse of the Bell and Necronomicurse.
-            CARD_COLOR.COLORLESS //The card color. If you're making your own character, it'll look something like this. Otherwise, it'll be CardColor.RED or something similar for a basegame character color.
+            "Star",
+            0,
+            CardType.ATTACK,
+            CardTarget.ENEMY,
+            CardRarity.SPECIAL,
+            CARD_COLOR.COLORLESS
     );
 
 
-    //This is theoretically optional, but you'll want it. The ID is how you refer to the card.
-    //For example, to add a card to the starting deck, you need to use its ID.
-    //With this, you can just use 'MyCard.ID'. Without it, you'd have to type out
-    //'yourModID:MyCard' and make sure you don't make any mistakes, and you'd also have to update it
-    //if you decided to change the card's ID.
     public static final String ID = makeID(cardInfo.baseId);
 
-    //These will be used in the constructor. Technically you can just use the values directly,
-    //but constants at the top of the file are easy to adjust.
     public static final int DAMAGE = 4;
     public static final int UPG_DAMAGE = 2;
 
     public Star() {
-        super(cardInfo); //Pass the cardInfo to the BaseCard constructor.
+        super(cardInfo);
 
-        setDamage(DAMAGE, UPG_DAMAGE); //Sets the card's damage and how much it increases when upgraded.
+        setDamage(DAMAGE, UPG_DAMAGE);
         this.exhaust = true;
     }
 
